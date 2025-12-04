e56abc487aa01f94ebae37f6a9a9a2163e6d71d0
uttam <uttamchaturvedi@gmail.com>
2025-12-04 21:36:28 +0100

    Added project and solution files

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Shopping.API.Models;
+using Shopping.API.Data;
+
+namespace Shopping.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            _logger.LogInformation("Fetching all products");
+            return Ok(GetProducts());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetById(string id)
+        {
+            var product = GetProducts().FirstOrDefault(p => p.Id == id);
+
+            if (product is null)
+            {
+                _logger.LogWarning("Product with id {ProductId} not found", id);
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        private IEnumerable<Product> GetProducts()
+        {
+            return ProductContext.Products;         
+
+        }
+    }
+}
---
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Shopping.Client.Models;
+
+namespace Shopping.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<HomeController> _logger;      
+
+        public HomeController(IHttpClientFactory httpClientFactory,ILogger<HomeController> logger)
+        {            
+            _logger = logger;
+            _httpClient = httpClientFactory.CreateClient("ShoppingAPIClient");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("/product");
+            var content = await response.Content.ReadAsStringAsync();
+            var productList = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
+            return View(productList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
